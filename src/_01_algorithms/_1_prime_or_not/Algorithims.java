4d85d3ab5d8e21ff7c3443e7aae709920cc7fd03
C2SDK <C2SDK@C47508>
2020-07-08 14:56:36 -0700

    Algorithims.java

--- src/_01_algorithms/_1_prime_or_not/Algorithims.java
+++ src/_01_algorithms/_1_prime_or_not/Algorithims.java
@@ -1,10 +1,15 @@
 package _01_algorithms._1_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class Algorithims {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String prime = JOptionPane.showInputDialog("What is your number?");
+int cody = Integer.parseInt(prime);
+for (int i = 1; i < 21; i++) {
+	if(i % 3==0&&i%5==0) {
 	}
 
 }
