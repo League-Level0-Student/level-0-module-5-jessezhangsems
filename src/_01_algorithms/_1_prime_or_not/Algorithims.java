c66b08f456982f61b6c41796248a4a1b5bca1bee
C2SDK <C2SDK@C47508>
2020-07-15 14:58:24 -0700

    fibonacci algorithi

--- src/_01_algorithms/_1_prime_or_not/Algorithims.java
+++ src/_01_algorithms/_1_prime_or_not/Algorithims.java
@@ -8,8 +8,16 @@ public class Algorithims {
 		// TODO Auto-generated method stub
 String prime = JOptionPane.showInputDialog("What is your number?");
 int cody = Integer.parseInt(prime);
-for (int i = 1; i < 21; i++) {
-	if(i % 3==0&&i%5==0) {
+for (int i = 2; i < cody; i++) {
+	if(cody % i==0) {
+	JOptionPane.showMessageDialog(null, "Your number is composite!");
+	System.exit(0);
 	}
-
+}
+	if(cody ==2) {
+	JOptionPane.showMessageDialog(null, "Your number is composite!");
+System.exit(0);
+}
+JOptionPane.showMessageDialog(null, "Your number is prime!");
+}
 }
---
+++ src/_01_algorithms/_2_fibonacci/fibonacci.java
@@ -0,0 +1,10 @@
+package _01_algorithms._2_fibonacci;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
