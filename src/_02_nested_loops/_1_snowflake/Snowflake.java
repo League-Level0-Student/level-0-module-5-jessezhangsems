a8175d4dd483846f86b2379ce78316405126848c
C2SDK <C2SDK@C47508>
2020-07-29 14:57:54 -0700

    For Loop S

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -1,56 +1,65 @@
-hi
+
 package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class Snowflake {
-	
+
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
 
+		Robot Squidward = new Robot();
+		Squidward.setX(300);
+		Squidward.setY(300);
+		Squidward.penDown();
+		Squidward.setSpeed(5);
+		for (int i = 0; i < 6; i++) {
+			Squidward.setPenColor(Color.black);
+			Squidward.move(50);
+			Squidward.turn(60);
+
+			if (i % 2 == 0) {
+				Squidward.setPenColor(Color.blue);
+			} else {
+				Squidward.setPenColor(Color.red);
+			}
+			for (int j = 0; j < 6; j++) {
+				Squidward.turn(-60);
+				Squidward.move(50);
+			}
+			Squidward.setPenColor(Color.black);
+		}
 
 		// #2. Set your robot’s position to x=300 and y=300
 
-
 		// #3. Put the robot's pen down
-
 
 		// #4. Set the robot’s speed to 5
 
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
+		// #6. Set the pen color to black
 
-			
-			// #6. Set the pen color to black
+		// #7. move the robot by 50
 
-			
-			// #7. move the robot by 50
+		// #8. turn the robot to the right by 60
 
-			
-			// #8. turn the robot to the right by 60
-			
-			
-			// #12. set the pen color to blue for even numbers of i
+		// #12. set the pen color to blue for even numbers of i
 
-			
-			// #13. set the pen color to red for all odd numbers of i
-			
-			
-			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
-		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
-			// #14. set the pen color to black
-				
-		
+		// #13. set the pen color to red for all odd numbers of i
+
+		// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
+
+		// #10. turn the robot to the left by 60
+
+		// #11. move the robot by 50 (play the start button)
+
+		// #14. set the pen color to black
+
 	}
 
 }
---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/For_Loop_Gauntlet.java
@@ -0,0 +1,13 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class For_Loop_Gauntlet {
+public static void main(String[] args) {
+	for (int i = 1; i < 100; i++) {
+		if (i%2==1) {
+			System.out.println(i);
+		}
+		
+	}
+	
+}
+}
